8218de33e5b0819d3a76b79ae62b435dcb304fad
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-01 16:28:16 +1300

    finish auth feature no tests yeet

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,33 +9,51 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { fetchStonks } from '../redux/actions/stonks'
 
 // Components
-import { Heading } from '@chakra-ui/react'
+import { Heading, Flex, Box } from '@chakra-ui/react'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
 
 function App () {
   const dispatch = useDispatch()
   const stonks = useSelector(state => state.stonks)
+  console.log(stonks)
 
   useEffect(() => {
     dispatch(fetchStonks())
   }, [])
 
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0()
-  console.log(isAuthenticated, user, loginWithRedirect, logout)
+  const { isAuthenticated, user } = useAuth0()
+
+  console.log(isAuthenticated, user)
   return (
     <>
       <header className="header">
-        <Heading as='h1'>Welcome to ESG stonks</Heading>
-        <LoginButton colorScheme='teal' />
-        <LogoutButton />
+        <Flex justify='space-between'>
+          <Box mr={4}>
+            <Heading as="h1" size="lg">
+              Stonks
+            </Heading>
+          </Box>
+          {isAuthenticated ? (
+            <LogoutButton colorScheme='teal' />
+          ) : (
+            <LoginButton colorScheme='teal' />
+          )}
+        </Flex>
       </header>
       <section className="main">
-
-        {stonks.map(stonk => {
-          return <div key={stonk.esg_id}>{stonk.company_name}</div>
-        })}
-
+        {isAuthenticated ? (
+          <>
+            <h2>Welcome {user.nickname}</h2>
+            <p>Your email is {user.email}</p>
+            <p>Here is some restricted content: </p>
+            {stonks.map(stonk => (
+              <p key={stonk.id}>{stonk.company_name}</p>
+            ))}
+          </>
+        ) : (
+          <p>Please log in to see your profile and restricted content</p>
+        )}
       </section>
     </>
   )
