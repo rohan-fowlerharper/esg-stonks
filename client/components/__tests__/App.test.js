5bb49d56c56b4559f0058d05ac71a24189377966
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-01 14:38:50 +1300

    add a passing test

--- client/components/__tests__/App.test.js
+++ client/components/__tests__/App.test.js
@@ -3,9 +3,9 @@ import { render, screen } from '@testing-library/react'
 
 import App from '../App'
 
-describe('hi', () => {
-  it('hello', () => {
+describe('App', () => {
+  it('works', () => {
     render(<App />)
-    expect(screen.getByText('stonks')).toBeInTheDocument()
+    expect(screen.getByText('Welcome to ESG stonks')).toBeInTheDocument()
   })
 })
