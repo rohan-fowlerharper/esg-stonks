80abd00ca3b1f753a2e9f829ef3d9f6399ffb0fe
Walid Bouladam <bouladam.walid86@gmail.com>
2021-12-01 16:35:05 +1300

    Added additional GET/ routes for name and symbol queries. Added GET/ route test for getStonks fn()

---
+++ server/routes/__tests__/stonks.test.js
@@ -0,0 +1,57 @@
+import request from 'supertest'
+
+const server = require('../../server')
+const db = require('../../db/stonks')
+
+jest.mock('../../db/stonks')
+
+describe('GET /api/v1/stonks', () => {
+  const fakeStonks = [{ 
+    id: 1, 
+    stockSymbol: 'FB', 
+    companyName: 'Facebook' 
+    }, 
+    { 
+    id:2 , 
+    stockSymbol: 'GOOG', 
+    companyName: 'Google' 
+  }]
+
+  beforeAll(() => {
+    db.getStonks.mockResolvedValue(fakeStonks)
+  })
+
+  it('calls db.getStonks', () => {
+    return request(server)
+      .get('/api/v1/stonks')
+      .expect(200)
+      .then(res => {
+      expect(db.getStonks).toHaveBeenCalled()
+      return null
+    })
+  })
+
+  it('returns all stonks from database', () => {
+    return request(server)
+      .get('/api/v1/stonks')
+      .expect(200)
+      .then(res => {
+        expect(res.body).toEqual(fakeStonks)
+        return null
+      })
+  })
+})
+
+describe('when database does not work', () => {
+  test('returns 500', () => {
+    expect.assertions(1)
+    const err = new Error('no worky')
+    db.getStonks.mockImplementation(() => Promise.reject(err))
+    return request(server)
+      .get('/api/v1/stonks')
+      .then(res => {
+        expect(res.status).toBe(500)
+        return null
+      })
+  })
+})
