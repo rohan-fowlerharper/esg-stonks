0a570b2d70c492e72d4b5d4f2958f888fb937016
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-01 16:44:14 +1300

    add basic passing tests

---
+++ client/redux/reducers/__tests__/stonks.test.js
@@ -0,0 +1,34 @@
+import stonks from '../stonks'
+import { fetchStonks } from '../../actions/stonks'
+
+it('has correct initial state', () => {
+  expect(stonks(undefined, { type: '@@INIT' })).toEqual([])
+})
+
+describe('SET_STONKS', () => {
+  const action = {
+    type: 'SET_STONKS',
+    stonks: [{
+      id: 1,
+      company_name: 'Stonk 1'
+    },
+    {
+      id: 2,
+      company_name: 'Stonk 2'
+    }]
+  }
+
+  it('sets the stonks', () => {
+    expect(stonks([], action)).toEqual(action.stonks)
+  })
+})
+
+describe('CLEAR_STONKS', () => {
+  const action = {
+    type: 'CLEAR_STONKS'
+  }
+
+  it('clears the stonks', () => {
+    expect(stonks([], action)).toEqual([])
+  })
+})
