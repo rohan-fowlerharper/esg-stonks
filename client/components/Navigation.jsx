ec11bcaa2795f65bf91fe92b8549f92ffbab1075
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-01 17:15:14 +1300

    figure out react router & chakra

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/react'
+// import { Link } from 'react-router-dom'
 
 function Navigation () {
   return (
     <>
       <Flex as="nav" w="100%">
         <img src="/images/esg-stonks_logo.png" />
+        {/* <Link as={}to='/'>Home</Link>
+        <Link to={'/companies'}>ESG</Link> */}
       </Flex>
     </>
   )
--- client/index.js
+++ client/index.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
-
 import App from './components/App'
 import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
     </Provider>,
     document.getElementById('app')
   )
