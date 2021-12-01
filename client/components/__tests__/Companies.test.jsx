cc055b59332dda468a1e062f22305a0be54e9370
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-02 09:48:58 +1300

    add component tests - skipped

---
+++ client/components/__tests__/Companies.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import Companies from '../pages/Companies'
+
+describe.skip('<Companies />', () => {
+  it('renders list of companies', () => {
+    render(<Companies />)
+    screen.debug()
+  })
+})
---
+++ client/components/__tests__/Home.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import Home from '../pages/Home'
+
+describe.skip('<Home />', () => {
+  it('renders stuff', () => {
+    render(<Home />)
+    const heading = screen.getAllByRole('heading')
+    expect(heading).toHaveTextContent('Welcome to ESG stonks')
+    screen.debug()
+  })
+})
