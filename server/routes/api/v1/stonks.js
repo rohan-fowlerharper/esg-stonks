c47bbffe4e0bbc3cbd7a8c3a6a9fa5b301e1894f
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-02 17:53:50 +1300

    Added GET/ goals route, goals api fn that fetches goals, completed all relevant testing of newly added functions/files

--- server/routes/api/v1/stonks.js
+++ server/routes/api/v1/stonks.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../../db/stonks')
 
+const { fetchGoalsByStockSymbol } = require('../../../util/goals')
+
 router.get('/', (req, res) => {
   db.getStonks()
     .then(stonks => {
@@ -29,6 +31,19 @@ router.get('/symbol/:symbol', (req, res) => {
   const symbol = req.params.symbol
   db.getStonkBySymbol(symbol)
     .then(stonk => res.json(stonk))
+    .catch(err => {
+      console.log(err)
+      res.status(500).send('There was an error')
+    })
+})
+
+router.get('/goals/:stockSymbol', (req, res) => {
+  // TODO
+  const stockSymbol = req.params.stockSymbol
+  fetchGoalsByStockSymbol(stockSymbol)
+    .then(goals => {
+      return res.json(goals)
+    })
     .catch(err => {
       console.log(err)
       res.status(500).send('There was an error')
