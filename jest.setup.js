23e00eab1cb75a17ea3b383f0e8b9d12357c8daa
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-02 18:10:10 +1300

    add environment variables to jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,4 @@
 require('@testing-library/jest-dom')
+process.env.ESG_API_KEY = 'api_key'
+process.env.ESG_API_HOST = 'esg-environmental-social-governance-data.p.rapidapi.com'
+process.env.ESG_API_URL = 'https://esg-environmental-social-governance-data.p.rapidapi.com'
