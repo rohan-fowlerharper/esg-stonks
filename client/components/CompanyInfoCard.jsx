574523f32583766ee84b6f9a4b896fb6f998225e
Walid Bouladam <bouladam.walid86@gmail.com>
2021-12-03 17:03:17 +1300

    Stashing for pull

--- client/components/CompanyInfoCard.jsx
+++ client/components/CompanyInfoCard.jsx
@@ -1,23 +1,47 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchStonks } from '../redux/actions/stonks'
-
-import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
-import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
-import { useTable, useSortBy } from 'react-table'
+import DataTable from 'react-data-table-component'
+// import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
+// import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+// import { useTable, useSortBy } from 'react-table'
 
 function CompanyInfoCard () {
   const dispatch = useDispatch()
   const stonks = useSelector(state => state.stonks)
-  const data = React.useMemo(() => [
-    { name: stonks.companyName}
-  ])
 
   useEffect(() => {
     dispatch(fetchStonks())
   }, [])
+
+  const columns = [
+    {
+      name: stonks.companyName,
+      selector: row => row.title
+    },
+    {
+      name: 'Year',
+      selector: row => row.year
+    }
+  ]
+
+  const data = [
+    {
+      id: 1,
+      title: 'Beetlejuice',
+      year: '1988'
+    },
+    {
+      id: 2,
+      title: 'Ghostbusters',
+      year: '1984'
+    }
+  ]
   return (
-   
+    <DataTable
+      columns={columns}
+      data={data}
+    />
   )
 }
 
