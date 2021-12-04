bcacff0b5692f5a466b7026ba699f354c0dcd312
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-04 15:09:23 +1300

    add responsive breakpoints and fix padding

---
+++ client/components/CompanyGridItem.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import {
+  GridItem,
+  Box,
+  Center,
+  Image,
+  useColorModeValue,
+  Flex,
+  Heading,
+  HStack,
+  Text
+} from '@chakra-ui/react'
+
+import RatingBar from './RatingBar'
+// CheckIcon and CloseIcon
+
+import ActiveStonkButton from './ActiveStonkButton'
+
+function CompanyGridItem ({ stonk }) {
+  const image = `https://s3.polygon.io/logos/${stonk.stockSymbol.toLowerCase()}/logo.png`
+
+  return (
+    <GridItem
+      role='company'
+      bg={useColorModeValue('white', 'gray.800')}
+      boxShadow={['base', 'xl', '2xl']}
+      rounded='lg'
+    >
+      <Box
+        roundedTop='lg'
+        h={36}
+        bg='white'
+        p={4}
+      >
+        <Center w='full' h='full'>
+          <Image
+            rounded={'sm'}
+            maxHeight='full'
+            width='auto'
+            src={image}
+            alt={stonk.stockSymbol}
+          />
+        </Center>
+      </Box>
+      <Box p={[2, 4, 6]}>
+        <Heading
+          as='h4'
+          fontSize={['md', 'lg', 'xl']}
+          fontWeight='semibold'
+          lineHeight='tight'
+          isTruncated
+        >
+          {stonk.companyName}
+        </Heading>
+        <Flex mt={2} justifyContent='space-between'>
+          <HStack justifyContent='flex-start'>
+            <Heading
+              as='h5'
+              fontSize={['sm', 'md', null]}
+              fontWeight='semibold'
+              lineHeight='tight'
+              color={useColorModeValue('gray.600', 'gray.400')}
+            >
+              {stonk.stockSymbol}
+            </Heading>
+            <Text color={useColorModeValue('gray.600', 'gray.400')}>|</Text>
+            <Heading
+              as='h5'
+              fontSize={['sm', 'md', null]}
+              fontWeight='semibold'
+              lineHeight='tight'
+              color={useColorModeValue('gray.600', 'gray.400')}
+            >
+              {stonk.exchangeSymbol}
+            </Heading>
+          </HStack>
+          {/* icon to be moved, todo: use closeicon and checkicon when it's selected */}
+          <ActiveStonkButton justifyContent='flex-end' stockSymbol={stonk.stockSymbol} />
+        </Flex>
+
+        <RatingBar rating={stonk.totalScore} max={3000} mt={4} />
+      </Box>
+    </GridItem>
+  )
+}
+
+export default CompanyGridItem
