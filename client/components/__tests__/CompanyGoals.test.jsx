0ecdacdcbd13057b2b6668237c50d08ac7154c31
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-06 15:29:24 +1300

    fix test

--- client/components/__tests__/CompanyGoals.test.jsx
+++ client/components/__tests__/CompanyGoals.test.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable jest/no-mocks-import */
 import '@testing-library/jest-dom'
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import { getGoals } from '../../apis/stonks'
 import CompanyGoals from '../CompanyGoals'
 
 jest.mock('../../apis/stonks')
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useEffect: jest.fn()
-}))
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useEffect: jest.fn()
+// }))
 // const MockCompanyGoals = ({ stockSymbol }) => {
 //   return (
 //     <Provider store={fakeStore}>
@@ -28,9 +29,14 @@ describe('<CompanyGoals />', () => {
   // useEffect.mockImplentation(React.useLayoutEffect)
   // React.useEffect.mockImplementation(f => f())
   it('should render the goals of the respective company', async () => {
-    render(<CompanyGoals stockSymbol='AAPL' />)
-    // screen.debug()
+    render(<ChakraProvider><CompanyGoals stockSymbol='AAPL' /></ChakraProvider>)
+    await waitFor(() => {
+      console.log(getGoals.mock.calls.length)
+      return getGoals.mock.calls.length > 0
+    })
+    console.log(getGoals.mock.calls.length)
     expect(screen.getAllByRole('list')[0]).toBeInTheDocument()
+    screen.debug()
     // TODO finish with useEffect properly mocked
   })
 })
