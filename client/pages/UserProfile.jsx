49d995eb314385f8670983347e012468247534fc
Walid Bouladam <bouladam.walid86@gmail.com>
2021-12-07 11:44:19 +1300

    Added edit profile btn and impported chakra form in order to edit user profile

--- client/pages/UserProfile.jsx
+++ client/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import { Heading, Container, Image, Text, Button, Flex, Link } from '@chakra-ui/react'
 import { useAuth0 } from '@auth0/auth0-react'
-import RegularLayout from '../layouts/RegularLayout'
 
 // import EditProfile from '../components/EditProfile'
 
@@ -8,21 +8,21 @@ function UserProfile () {
   const { user, isAuthenticated } = useAuth0()
 
   return (
-    <RegularLayout>
-      <h1>User Profile</h1>
+    <Container maxW={'5xl'} centerContent>
       {isAuthenticated && (
         <>
-          <img src={user.picture} alt="Profile" />
-          <h2>Welcome, {user.name}!</h2>
-          <p>{user.email}</p>
-          {/* <button onClick={() => {
-            console.log('clicked')
-          }}>Edit Profile</button>
-          <EditProfile /> */}
+          <Image src={user.picture} alt="Profile" />
+          <Text> Welcome, {user.name}!</Text>
+          <Button variantColor="teal" onClick={() => console.log('clicked')}>
+            <Link href="/edit-profile">
+              Edit Profile
+            </Link>
+          </Button>
           {!isAuthenticated && <p>Please log in to view profile</p>}
         </>
       )}
-    </RegularLayout>
+    </Container>
+
   )
 }
 
