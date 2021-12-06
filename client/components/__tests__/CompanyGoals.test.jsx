aacbb7e8b2a64ea3646aff2dcedb7b0c85d455b6
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-06 15:07:59 +1300

    an attempt

--- client/components/__tests__/CompanyGoals.test.jsx
+++ client/components/__tests__/CompanyGoals.test.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable jest/no-mocks-import */
 import '@testing-library/jest-dom'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 
 import { getGoals } from '../../apis/stonks'
 import CompanyGoals from '../CompanyGoals'
+import { act } from 'react-dom/test-utils'
 
 jest.mock('../../apis/stonks')
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useEffect: jest.fn()
+}))
 // const MockCompanyGoals = ({ stockSymbol }) => {
 //   return (
 //     <Provider store={fakeStore}>
@@ -17,7 +23,12 @@ jest.mock('../../apis/stonks')
 describe('<CompanyGoals />', () => {
   getGoals.mockImplementation(() => Promise.resolve([{ sasb: 'goal1', sdg: 'goal1' }, { sasb: 'goal2', sdg: 'goal2' }, { sasb: 'goal3', sdg: 'goal3' }])
   )
-  it('should render the goals of the respective company', () => {
-    render(<CompanyGoals stockSymbol={'AAPL'} />)
+  // useEffect.mockImplentation(React.useLayoutEffect)
+  // React.useEffect.mockImplementation(f => f())
+  it('should render the goals of the respective company', async () => {
+    render(<CompanyGoals stockSymbol='AAPL' />)
+    // screen.debug()
+    expect(screen.getAllByRole('list')[0]).toBeInTheDocument()
+    // TODO finish with useEffect properly mocked
   })
 })
