8f54124d193b638d6c6c40d6ad9a347f4a01ee3a
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-08 11:56:08 +1300

    extract vars to .env

--- server/util/auth.js
+++ server/util/auth.js
@@ -1,16 +1,19 @@
 const jwt = require('express-jwt')
 const jwks = require('jwks-rsa')
+require('dotenv').config()
 
-// I would extract core variables into a .env file so the app can be configured without making a commit
+const tenantUrl = process.env.AUTH0_TENANT_URL
+const audienceUrl = process.env.AUTH0_AUDIENCE_URL
+
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: 'https://aihe-2021-rohan.au.auth0.com/.well-known/jwks.json'
+    jwksUri: `${tenantUrl}/.well-known/jwks.json`
   }),
-  audience: 'https://esg-stonks/users',
-  issuer: 'https://aihe-2021-rohan.au.auth0.com/',
+  audience: audienceUrl,
+  issuer: `${tenantUrl}/`,
   algorithms: ['RS256']
 })
 
