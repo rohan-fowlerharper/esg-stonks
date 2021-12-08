7cd5f41442deb232df869ff96de6235781cd911e
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-08 16:59:08 +1300

    add favourite button functionality

--- client/components/FavouriteStonkButton.jsx
+++ client/components/FavouriteStonkButton.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import { AiOutlineStar, AiFillStar } from 'react-icons/ai'
+import React, { useEffect } from 'react'
+import { AiOutlineStar } from 'react-icons/ai'
+import { IconButton } from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons'
 import { useAuth0 } from '@auth0/auth0-react'
-import { addUserFavourite, removeUserFavourite } from '../apis/stonks'
-import { useSelector } from 'react-redux'
+import { addFavouriteStonk, removeFavouriteStonk } from '../redux/actions/favouriteStonks'
+import { useDispatch, useSelector } from 'react-redux'
 
-function FavouriteStonkButton ({ stonkId }) {
+function FavouriteStonkButton ({ stonkId, ...rest }) {
   const { getAccessTokenSilently } = useAuth0()
+  const [isLoading, setIsLoading] = React.useState(false)
   const isFavourite = useSelector(state => state.favouriteStonks.includes(stonkId))
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    setIsLoading(false)
+  }, [isFavourite])
 
   async function handleClick () {
+    setIsLoading(true)
     const token = await getAccessTokenSilently()
     if (isFavourite) {
-      await removeUserFavourite(stonkId, token)
+      dispatch(removeFavouriteStonk(stonkId, token))
     } else {
-      await addUserFavourite(stonkId, token)
+      dispatch(addFavouriteStonk(stonkId, token))
     }
+  }
+
+  const sharedProps = {
+    colorScheme: 'pink',
+    role: 'toggle-favourite-stonk',
+    rounded: 'full',
+    size: 'xs',
+    onClick: handleClick,
+    ...rest
   }
 
   return (
     <>
-      {isFavourite ? (
-        <AiFillStar onClick={handleClick} />
+      {isLoading ? (
+        <IconButton isLoading variant='outline' {...sharedProps} />
+      ) : isFavourite ? (
+        <IconButton
+          icon={<CheckIcon />}
+          {...sharedProps}
+        />
       ) : (
-        <AiOutlineStar onClick={handleClick} />
-      )}
+        <IconButton
+          variant='outline'
+          icon={<AiOutlineStar />}
+          {...sharedProps}
+        />)}
     </>
   )
 }
