331eed58d8cf08b6257a6b51c4d69e91c5522d46
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-08 14:55:38 +1300

    refactor test to include esg level - passing

--- client/components/__tests__/RatingBar.test.js
+++ client/components/__tests__/RatingBar.test.js
@@ -8,18 +8,18 @@ expect.extend(matchers)
 
 describe('<RatingBar />', () => {
   it('should render the component', () => {
-    const { getByRole } = render(<RatingBar max={1000} rating={500} />)
+    const { getByRole } = render(<RatingBar max={1000} rating={500} level={'Excellent'} />)
     expect(getByRole('rating-bar')).toBeInTheDocument()
   })
 
   it('should render the component with correct width and colour', () => {
-    render(<RatingBar max={1000} rating={1000} />)
+    render(<RatingBar max={1000} rating={1000} level={'Excellent'} />)
     const ratingBar = screen.getByRole('rating-bar')
     expect(ratingBar.firstChild).toHaveStyleRule('width', '100%')
     expect(ratingBar.firstChild).toHaveStyleRule('background', 'green.500')
   })
   it('should render with different rating/max', () => {
-    render(<RatingBar max={100} rating={17} />)
+    render(<RatingBar max={100} rating={17} level={'Low'} />)
     const ratingBar = screen.getByRole('rating-bar')
     expect(ratingBar.firstChild).toHaveStyleRule('width', '17%')
     expect(ratingBar.firstChild).toHaveStyleRule('background', 'red.500')
