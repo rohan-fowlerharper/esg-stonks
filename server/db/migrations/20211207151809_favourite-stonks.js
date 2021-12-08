c9e63c6423da00bd26b4b6b7a398bab433b97a95
Rohan Fowler-Harper <88605753+rohan-fowlerharper@users.noreply.github.com>
2021-12-08 19:33:01 +1300

    fix seeding

--- server/db/migrations/20211207151809_favourite-stonks.js
+++ server/db/migrations/20211207151809_favourite-stonks.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('favourite_stonks', t => {
     t.increments('id').unique().primary()
     t.text('user_id')
-    t.integer('stonk_id').references('id').inTable('stonks')
+    t.integer('stonk_id').notNullable().references('id').inTable('stonks').onDelete('cascade')
   })
 }
 
