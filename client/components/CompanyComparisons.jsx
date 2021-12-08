609444ded0924883173dee5d019136fc4177b802
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-08 16:06:59 +1300

    add company goals heading & description

--- client/components/CompanyComparisons.jsx
+++ client/components/CompanyComparisons.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, SimpleGrid, Box } from '@chakra-ui/react'
+import { Container, SimpleGrid, Box, Heading, Center, Text, useColorModeValue } from '@chakra-ui/react'
 
 import CompanyRadar from './CompanyRadar'
 import CompanyInfoCard from './CompanyInfoCard'
@@ -29,6 +29,18 @@ function Comparison ({ stonks, activeStonks: activeSymbols }) {
           <CompanyRadar stonk1={activeStonk1} stonk2={activeStonk2} width='full' height='500px' />
         </Container>
 
+        <Center>
+          <Heading as='h2'>
+          SASB &amp; SDG Goals
+          </Heading>
+        </Center>
+        <br/>
+        <Center>
+          <Text mb={4} fontSize='xl' color={useColorModeValue('gray.800', 'gray.300')}>
+        Sustainability goals pledged by companies are used to evaluate the above ESG scores
+          </Text>
+        </Center>
+        <br/>
         <SimpleGrid
           columns={2}
           gap={[2, 4, 6]}
